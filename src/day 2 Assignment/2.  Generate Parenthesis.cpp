165a0830e29ddd55607f95e3198a271715262625
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-02-18 12:04:14 +0530

    Update 2.  Generate Parenthesis.cpp

--- src/day 2 Assignment/2.  Generate Parenthesis.cpp
+++ src/day 2 Assignment/2.  Generate Parenthesis.cpp
@@ -1,1 +1,45 @@
 
+# include<iostream> 
+# define M_S 100 
+
+void print_Parenthesis(int pos, int n, int open, int close); 
+
+void printParenthesis(int n) 
+{ 
+	if(n > 0) 
+		print_Parenthesis(0, n, 0, 0); 
+	return; 
+}	 
+
+void print_Parenthesis(int pos, int n, int open, int close) 
+{ 
+	static char str[M_S];	 
+	
+	if(close == n) 
+	{ 
+		printf("%s \n", str); 
+		return; 
+	} 
+	else
+	{ 
+		if(open > close) 
+		{ 
+			str[pos] = ')'; 
+			print_Parenthesis(pos+1, n, open, close+1); 
+		} 
+		
+		if(open < n) 
+		{ 
+		str[pos] = '('; 
+		print_Parenthesis(pos+1, n, open+1, close); 
+		} 
+	} 
+} 
+
+int main() 
+{ 
+	int n = 3; 
+	printParenthesis(n); 
+	
+	return 0; 
+} 
