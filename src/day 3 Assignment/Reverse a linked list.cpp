e7dd6be6398b55e3437b1b56c8bb90c3a990ecd6
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-02-19 15:59:42 +0530

    Update Reverse a linked list.cpp

--- src/day 3 Assignment/Reverse a linked list.cpp
+++ src/day 3 Assignment/Reverse a linked list.cpp
@@ -1,1 +1,69 @@
 
+#include <iostream>
+
+using namespace std;
+
+struct node {
+    int data;
+    struct node *link;
+    
+};
+
+struct node* addEnd(struct node *ptr, int data)
+{
+    struct node *current = (struct node *)malloc(sizeof(struct node));
+    current->data = data;
+    current->link = NULL;
+    
+    ptr->link = current;
+    return current;
+    
+}
+
+static void reverse(struct node** head)
+{
+    struct node* prev = NULL;
+    struct node* current = *head;
+    struct node* link = NULL;
+    
+    while (current != NULL) {
+       
+        link = current->link;
+        current->link = prev;
+        prev = current;
+        current = link;
+    }
+    *head = prev;
+}
+
+static void printNode(struct node *head)
+{
+    struct node *ptr = head;
+    while(ptr != NULL)
+    {
+        cout<<"After reverse : "<<ptr->data<<endl;
+        ptr = ptr->link;
+    }
+}
+int main()
+{
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    head->data = 10;
+    head->link = NULL;
+    
+    struct node *ptr = head;
+    ptr = addEnd(ptr,20);
+    ptr = addEnd(ptr,30);
+    
+    ptr = head;
+    while(ptr != NULL)
+    {
+        cout<<"Before reverse : "<<ptr->data<<endl;
+        ptr = ptr->link;
+    }
+
+    reverse(&head);
+    printNode(head);
+    
+    return 0;
+}
